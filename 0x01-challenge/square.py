fa8760de82474f782270668ae8ae54f43adb7133
Athena Deng <539@holbertonschool.com>
2019-05-27 05:38:21 +0000

    reverted changes

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+"""MODULE DOCSTRING"""
 
-class square():
-    
-    width = 0
-    height = 0
 
-    
+class Square():
+    """a Square class"""
+
     def __init__(self, *args, **kwargs):
+        """making a new instance of Square object"""
+        self.width = 0
+        self.height = 0
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,14 +17,16 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """perimeter of square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """returns width and height of square"""
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
