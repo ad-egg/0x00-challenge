9563bf3d2b67dc042515d99c94904e0f541e6c87
Athena Deng <539@holbertonschool.com>
2019-05-27 05:40:53 +0000

    renamed method

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -16,7 +16,7 @@ class Square():
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def perimeter_of_my_square(self):
         """perimeter of square"""
         return (self.width * 2) + (self.height * 2)
 
@@ -25,8 +25,7 @@ class Square():
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.perimeter_of_my_square())
