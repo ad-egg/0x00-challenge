a3ac2e0e289ae6fd1a3682a0f70aeea7969a2ae0
Athena Deng <539@holbertonschool.com>
2019-05-27 05:34:22 +0000

    changed area method return value

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 
-
 class square():
+    
+    width = 0
+    height = 0
 
+    
     def __init__(self, *args, **kwargs):
-        self.width = 0
-        self.height = 0
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def PermiterOfMySquare(self):
         return (self.width * 2) + (self.height * 2)
